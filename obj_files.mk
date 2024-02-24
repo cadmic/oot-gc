acf87b72bcd7236a5b5217681a3bd5c7e0b6dcb9
Ticamus <nassib.rayane@gmail.com>
2024-02-24 20:48:09 +0100

    started file.s

--- obj_files.mk
+++ obj_files.mk
@@ -17,7 +17,7 @@ O_FILES :=                                  \
     $(BUILD_DIR)/asm/mcardGCN.o             \
     $(BUILD_DIR)/src/codeGCN.o              \
     $(BUILD_DIR)/asm/soundGCN.o             \
-    $(BUILD_DIR)/asm/frame.o                \
+    $(BUILD_DIR)/src/frame.o                \
     $(BUILD_DIR)/src/system.o               \
     $(BUILD_DIR)/src/cpu.o                  \
     $(BUILD_DIR)/asm/pif.o                  \
