e3d56eac05409752eaaec3f30efef8259ec052a3
cadmic <cadmic24@gmail.com>
2024-06-03 13:02:23 -0700

    WIP mem

--- libc/string.h
+++ libc/string.h
@@ -1,14 +1,20 @@
 #ifndef _STRING_H_
 #define _STRING_H_
 
-#include "mem_funcs.h"
 #include "stddef.h"
 
-size_t strlen(const char* s);
-char* strcpy(char* dest, const char* src);
-char* strncpy(char* dest, const char* src, size_t num);
+void* memcpy(void* dst, const void* src, size_t n);
+void* memmove(void* dst, const void* src, size_t n);
+char* strcpy(char* dst, const char* src);
+char* strncpy(char* dst, const char* src, size_t num);
+
+int memcmp(const void* p1, const void* p2, size_t n);
 int strcmp(const char* s1, const char* s2);
 int strncmp(const char* s1, const char* s2, size_t n);
-char* strncat(char* dest, const char* src, size_t n);
+
+void* memchr(const void* p, int val, size_t n);
+void* memset(void* dst, int val, size_t n);
+
+size_t strlen(const char* s);
 
 #endif
